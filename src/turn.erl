6d3717fd0068e25a0517439db53e01a9fb354b82
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-01-27 11:23:16 +0100

    Revert "Handle case, when parent resolver returns an error"
    This reverts commit 2338ee209298af0b27cfed1b22d9e7f188914d4f.

--- src/turn.erl
+++ src/turn.erl
@@ -74,7 +74,7 @@
          rcvd_bytes = 0 :: non_neg_integer(), rcvd_pkts = 0 :: non_neg_integer(),
          sent_bytes = 0 :: non_neg_integer(), sent_pkts = 0 :: non_neg_integer(),
          parent :: pid() | undefined, parent_resolver :: function() | undefined,
-         port_black_list = [] :: list(), start_timestamp = get_timestamp() :: integer(),
+         start_timestamp = get_timestamp() :: integer(),
          candidate_addr :: {inet:ip4_address(), inet:port_number()} | undefined,
          server_pid :: pid()}).
 
@@ -119,7 +119,6 @@ init([Opts]) ->
                server_name = proplists:get_value(server_name, Opts),
                parent = proplists:get_value(parent, Opts),
                parent_resolver = proplists:get_value(parent_resolver, Opts),
-               port_black_list = [],
                server_pid = proplists:get_value(server_pid, Opts),
                username = Username,
                realm = Realm,
@@ -568,14 +567,20 @@ is_stun_packet(_Pkt) ->
     false.
 
 send_payload_to_parent(State, Payload) ->
-    NewState = try_resolve_parent(State),
+    NewState =
+        case State#state.parent of
+            undefined ->
+                {_Addr, Port} = State#state.candidate_addr,
+                {ok, Parent} = (State#state.parent_resolver)(Port),
+                State#state{parent = Parent};
+            _Parent ->
+                State
+        end,
 
-    case {NewState#state.parent, is_stun_packet(Payload)} of
-        {undefined, _IsStunPacket} ->
-            pass;
-        {Parent, true} ->
+    case is_stun_packet(Payload) of
+        true ->
             {ok, StunMsg} = stun_codec:decode(Payload, datagram),
-            Parent
+            NewState#state.parent
             ! {connectivity_check,
                [{class, StunMsg#stun.class},
                 {magic, StunMsg#stun.magic},
@@ -586,28 +591,10 @@ send_payload_to_parent(State, Payload) ->
                 {ice_controlled, StunMsg#stun.'ICE-CONTROLLED'},
                 {ice_controlling, StunMsg#stun.'ICE-CONTROLLING'}],
                self()};
-        {Parent, false} ->
-            Parent ! {ice_payload, Payload}
+        false ->
+            NewState#state.parent ! {ice_payload, Payload}
     end,
     NewState.
-
-try_resolve_parent(#state{parent = undefined} = State) ->
-    {_Addr, Port} = State#state.candidate_addr,
-
-    case lists:member(Port, State#state.port_black_list) of
-        true ->
-            State;
-        false ->
-            case (State#state.parent_resolver)(Port) of
-                {ok, Parent} ->
-                    State#state{parent = Parent};
-                {error, _Reason} ->
-                    NewPortBlackList = [Port] ++ State#state.port_black_list,
-                    State#state{port_black_list = NewPortBlackList}
-            end
-    end;
-try_resolve_parent(State) ->
-    State.
 
 time_left(TRef) ->
     erlang:read_timer(TRef) div 1000.
